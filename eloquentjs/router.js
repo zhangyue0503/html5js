6661aa800a6a07a9e3c6b21d414d4d61c4ee2d06
zhangyue0503 <zhangyue0503@hotmail.com>
2016-12-09 00:21:58 +0800

    20161208

---
+++ eloquentjs/router.js
@@ -0,0 +1,28 @@
+/**
+ * Created by zhangyue on 2016/12/8.
+ */
+
+var Router = module.exports = function(){
+    this.routes = [];
+};
+
+Router.prototype.add = function(method,url,handler){
+    this.routes.push({method:method,
+    url:url,
+    handler:handler});
+};
+
+Router.prototype.resolve = function(request,response){
+    var path = require("url").parse(request.url).pathname;
+
+    return this.routes.some(function(route){
+        var match = route.url.exec(path);
+        // console.log(match,route.method,request.method);
+        if(!match || route.method != request.method)
+            return false;
+        var urlParts = match.slice(1).map(decodeURIComponent);
+        route.handler.apply(null,[request,response].concat(urlParts));
+        return true;
+    });
+};
+
