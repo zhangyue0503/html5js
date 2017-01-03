658046418743e06e4caa08d9d5f25397d5deec6e
zhangyue0503 <zhangyue0503@hotmail.com>
2017-01-03 21:52:23 +0800

    20170103

--- speakingjavascript/18.js
+++ speakingjavascript/18.js
@@ -16,3 +16,14 @@ console.log(countElements(['a',,'b']));
 //使用apply()创建包含undefined元素的数组
 console.log(Array.apply(null,Array(3)));
 
+//reduce
+function printArgs(prev,cur,i){
+    console.log('prev:'+prev+',cur:'+cur+',i:'+i);
+    return prev+cur;
+}
+
+['a','b','c'].reduce(printArgs);
+['a','b','c'].reduce(printArgs,'x');
+
+['a','b','c'].reduceRight(printArgs);
+['a','b','c'].reduceRight(printArgs,'x');
