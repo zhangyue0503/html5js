ade79f49d9a7623eca56adbb18f88e48d57ec4c4
zhangyue0503 <zhangyue0503@hotmail.com>
2017-05-01 23:46:41 +0800

    20170501

---
+++ nodejsinaction/5/5.1.js
@@ -0,0 +1,60 @@
+/**
+ * Created by zhangyue on 2017/5/1.
+ */
+
+//基于文件的存储
+var fs              = require('fs'),
+	path            = require('path'),
+	args            = process.argv.splice(2),
+	command         = args.shift(),
+	taskDescription = args.join(' '),
+	file            = path.join(process.cwd(), '/.tasks');
+
+switch (command) {
+	case 'list':
+		listTasks(file);
+		break;
+	case 'add':
+		addTask(file, taskDescription);
+		break;
+	default:
+		console.log('Usage: ' + process.argv[0] + ' list|add [taskDescription]');
+}
+
+function loadOrInitializeTaskArray(file, cb) {
+	fs.exists(file, function (exists) {
+		var tasks = [];
+		if (exists) {
+			fs.readFile(file, 'utf8', function (err, data) {
+				if (err) throw err;
+				var data = data.toString();
+				tasks    = JSON.parse(data || '[]');
+				cb(tasks);
+			});
+		} else {
+			cb([]);
+		}
+	});
+}
+
+function listTasks(file) {
+	loadOrInitializeTaskArray(file, function (tasks) {
+		for (var i in tasks) {
+			console.log(tasks[i]);
+		}
+	});
+}
+
+function storeTasks(file, tasks) {
+	fs.writeFile(file, JSON.stringify(tasks), 'utf8', function (err) {
+		if (err) throw err;
+		console.log('Saved.');
+	});
+}
+
+function addTask(file, taskDescription) {
+	loadOrInitializeTaskArray(file, function (tasks) {
+		tasks.push(taskDescription);
+		storeTasks(file, tasks);
+	});
+}
---
+++ nodejsinaction/5/timetrack_server.js
@@ -0,0 +1,59 @@
+/**
+ * Created by zhangyue on 2017/5/1.
+ */
+
+//mysql
+var http = require('http'),
+	work = require('./timetrack'),
+	mysql = require('mysql');
+
+var db = mysql.createConnection({
+	host:'localhost',
+	user:'root',
+	password:'',
+	database:'timetrack'
+});
+
+var server = http.createServer(function(req,res){
+	switch(req.method){
+		case 'POST':
+			switch(req.url){
+				case '/':
+					work.add(db,req,res);
+					break;
+				case '/archive':
+					work.archive(db,req,res);
+					break;
+				case '/delete':
+					work.delete(db,req,res);
+					break;
+
+			}
+			break;
+		case 'GET':
+			switch (req.url){
+				case '/':
+					work.show(db,res);
+					break;
+				case '/archived':
+					work.showArchived(db,res);
+					break;
+			}
+			break;
+	}
+});
+
+db.query(
+	"CREATE TABLE IF NOT EXISTS work("
+	+"id INT(10) NOT NULL AUTO_INCREMENT,"
+	+"hours DECIMAL(5,2) DEFAULT 0,"
+	+"date DATE,"
+	+"archived INT(1) DEFAULT 0,"
+	+"description LONGTEXT,"
+	+"PRIMARY KEY(id))",
+	function(err){
+		if(err) throw err;
+		console.log('Server started...');
+		server.listen('3000');
+	}
+);
