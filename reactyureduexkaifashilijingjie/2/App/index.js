8fe1c86e404049b595b6f686cf1241fa8f4b86f4
zhangyue0503 <zhangyue0503@hotmail.com>
2017-06-26 23:48:31 +0800

    20170626

---
+++ reactyureduexkaifashilijingjie/2/App/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function App(){
+	return <h1>hello world!</h1>;
+};
+
+// export default class App extends React.Component{
+// 	render(){
+// 		return (
+// 			<h1>hello world!</h1>
+// 		);
+// 	};
+// };
