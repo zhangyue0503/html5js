e05372c088391fe8af77fec78b50345b62198cdf
zhangyue0503 <zhangyue0503@hotmail.com>
2017-07-15 17:30:55 +0800

    20170715

---
+++ jiemiangular2/4/webpack.config.js
@@ -0,0 +1,15 @@
+var webpack = require('webpack');
+var path = require('path');
+module.exports = {
+	entry:'./src/main.ts',
+	output:{
+		filename:'bundle.js'
+	},
+	resolve:{
+		root:[path.join(__dirname,'src')],
+		extensions:['','.ts','.js']
+	},
+	module:{
+		loaders:[{test:/\.ts$/,loaders:['ts-loader']}]
+	}
+};
