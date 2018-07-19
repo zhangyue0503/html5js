324b6dc74162b83adcafe2d384ffa87da7a93f09
zhangyue0503 <zhangyue0503@hotmail.com>
2018-07-19 10:35:04 +0800

    20180719

--- nodejs系列/4/lg.js
+++ nodejs系列/4/lg.js
@@ -3,10 +3,20 @@ var http = require('http'),
     urlLib = require('url'),
     querystring = require('querystring');
 
+var vip = {
+    'Ly':123
+};
+
 http.createServer(function (req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
 
     var json = urlLib.parse(req.url, true).query;
+
+    if(vip[json.user]){
+        res.write(`用户名已存在`);
+        res.end();
+        return;
+    }
 
     fs.readFile("./帐号密码.txt", 'utf8', function (err, data) {
         if (err) {
@@ -40,6 +50,13 @@ http.createServer(function (req, res) {
 
     var json = urlLib.parse(req.url, true).query;
 
+    if(vip[json.user] == json.pass){
+        res.write('大人您可回来了');
+        res.end();
+        return;
+    }
+
+
     fs.readFile("./帐号密码.txt", 'utf8', function (err, data) {
         if (err) {
             console.log(err);
@@ -56,3 +73,112 @@ http.createServer(function (req, res) {
     });
 
 }).listen(9282);
+
+http.createServer(function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    var json = urlLib.parse(req.url, true).query;
+
+    fs.readFile("./文章列表.txt", 'utf8', function (err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            var arrData = eval('(' + data + ')');
+            console.log(arrData);
+            arrData.push(json.fileName);
+            fs.writeFile('./文章列表.txt',JSON.stringify(arrData),function(err){
+                if(err){
+                console.log(err);
+                }else{
+                    res.write('发布成功');
+                    res.end();
+
+                    fs.writeFile('./文章列表/'+json.fileName+'.txt',json.inner,function(){
+                        if(err){
+                            console.log(err);
+                        }
+                    });
+                }
+                
+            });
+            
+        }
+    });
+
+}).listen(9283);
+
+http.createServer(function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    fs.readFile("./文章列表.txt", 'utf8', function (err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.write(data);
+            res.end();
+            
+        }
+    });
+
+}).listen(9284);
+
+http.createServer(function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    var json = urlLib.parse(req.url, true).query;
+
+    fs.readFile("./文章列表/"+json.fileName+".txt", 'utf8', function (err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.write(data);
+            res.end();
+        }
+    });
+
+}).listen(9285);
+
+
+http.createServer(function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    fs.readFile("./帐号密码.txt", 'utf8', function (err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            var allData = eval('('+data+')');
+            var arr = [];
+            for(var i in allData){
+                arr.push(i);
+            }
+            res.write(JSON.stringify(arr));
+            res.end();
+            
+        }
+    });
+
+}).listen(9290);
+
+http.createServer(function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    var json = urlLib.parse(req.url, true).query;
+    fs.readFile("./帐号密码.txt", 'utf8', function (err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            var allData = eval('('+data+')');
+            delete allData[json.deleteName];
+            fs.writeFile("./帐号密码.txt",JSON.stringify(allData),function(err){
+                if(err){
+                    console.log(err);
+                }else{
+                    res.write('删除成功');
+                    res.end();
+                }
+            });
+            
+            
+        }
+    });
+
+}).listen(9300);
